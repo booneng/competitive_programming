0160b8dcbf76163715bea67cbfcd20dbbc65bd53
booneng <ohbooneng@gmail.com>
2018-06-05 10:39:25 +0800

    inversions

---
+++ codeforces/986B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    a[i]--;
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] == -1) continue;
+    ans ^= 1;
+    int x = i;
+    while (x != -1) {
+      int y = a[x];
+      a[x] = -1;
+      x = y;
+    }
+  }
+
+  if (ans) {
+    cout << "Um_nik";
+  }
+  else {
+    cout << "Petr";
+  }
+}
