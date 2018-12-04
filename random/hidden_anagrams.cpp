f6598d0e375606c9f53835d47d2d6feee9051016
booneng <ohbooneng@gmail.com>
2018-12-04 22:29:08 +0800

    improve answer

--- random/hidden_anagrams.cpp
+++ random/hidden_anagrams.cpp
@@ -1,19 +1,29 @@
+// this program finds the longest anagramatically equivalent substring of s1 and s2
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-
+namespace std { 
+  template<> struct hash<pair<int, int> > { 
+    size_t operator() (const pair<int, int>& key) const {
+      return hash<int>()(key.first) + hash<int>()(key.second);
+    }
+  };
+}
+  
 
 int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
   string s1;
   string s2;
   cin >> s1 >> s2;
-  const vector<int> primes = {179426003, 179426453,
-			      179425019, 179425559, 179426029, 179426491,
-			      179425027, 179425579, 179426081, 179426549};
+  if (s1.size() > s2.size()) swap(s1, s2);
+  const vector<int> primes = {1000000007, 1000000009};
     
   int psize = primes.size();
-  vector<bitset<180000000>> table(psize);
 
   int n = 4001;
   vector<vector<int>> powers(psize, vector<int>(26));
@@ -24,8 +34,8 @@ int main() {
       cur = (cur * n) % primes[i];
     }
   }
-  vector<vector<int>> asums1(psize, vector<int>(s1.size() + 1));
-  vector<vector<int>> asums2(psize, vector<int>(s2.size() + 1));
+  vector<vector<int>> asums1(psize, vector<int>(s1.size() + 1, 0));
+  vector<vector<int>> asums2(psize, vector<int>(s2.size() + 1, 0));
   for (int i = 0; i < primes.size(); i++) {
     for (int j = 0; j < s1.size(); j++) {
       asums1[i][j + 1] = (asums1[i][j] + powers[i][s1[j] - 'a']) % primes[i];
@@ -34,31 +44,32 @@ int main() {
       asums2[i][j + 1] = (asums2[i][j] + powers[i][s2[j] - 'a']) % primes[i];
     }
   }
-  for (int i = 0; i < s1.size(); i++) {
-    for (int j = i; j < s1.size(); j++) {
-      for (int k = 0; k < psize; k++) {
-  	long long hash = asums1[k][j + 1] - asums1[k][i];
-  	if (hash < 0) hash += primes[k];
-  	table[k].set(hash);
-      }
-    }
-  }
+
+  
   int ans = 0;
-  for (int i = 0; i < s2.size(); i++) {
-    for (int j = i; j < s2.size(); j++) {
-      int len = j + 1 - i;
-      if (len <= ans) continue;
-      bool contains = true;
-      for (int k = 0; k < psize; k++) {
-  	long long hash = asums2[k][j + 1] - asums2[k][i];
-  	if (hash < 0) hash += primes[k];
-  	if (!table[k][hash]) {
-  	  contains = false;
-  	  break;
-  	}
-      }
-      if (contains) ans = len;
+  for (int len = s1.size(); len >= 1; len--) {
+    unordered_set<pair<int, int>> table;
+    for (int i = 0; i <= s1.size() - len; i++) {
+      int j = i + len;
+      int hash1 = asums1[0][j] - asums1[0][i];
+      int hash2 = asums1[1][j] - asums1[1][i];
+      if (hash1 < 0) hash1 += primes[0];
+      if (hash2 < 0) hash2 += primes[1];
+      table.insert({hash1, hash2});
     }
+    
+    for (int i = 0; i <= s2.size() - len; i++) {
+      int j = i + len;
+      int hash1 = asums2[0][j] - asums2[0][i];
+      int hash2 = asums2[1][j] - asums2[1][i];
+      if (hash1 < 0) hash1 += primes[0];
+      if (hash2 < 0) hash2 += primes[1];
+      if (table.find({hash1, hash2}) != table.end()) {
+	ans = len;
+	break;
+      }
+    }
+    if (ans) break;
   }
   cout << ans << endl;
 }
