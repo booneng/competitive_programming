06516967b3b8138b83f74862f66d4aeccec7cf2e
booneng <ohbooneng@gmail.com>
2019-01-07 19:39:54 +0800

    nice table

--- codeforces/1099E.cpp
+++ codeforces/1099E.cpp
@@ -2,67 +2,89 @@
 
 using namespace std;
 
-string letters = "AGTC";
-
-vector<string> get_permutations(int n) {
-  if (n == 0) {
-    string s;
-    vector<string> res;
-    res.push_back(s);
-    return res;
-  }
-  vector<string> prev = get_permutations(n - 1);
-  vector<string> res;
-  for (string s : prev) {
-    for (char c : letters) {
-      bool can = true;
-      for (char sc : s) {
-        if (sc == c) {
-          can = false;
-          break;
-        }
-      }
-      if (can) {
-        string tmp = c + s;
-        res.push_back(tmp);
-      }
-    }
-  }
-  return res;
-}
-
-
 int main() {
   int n, m;
   cin >> n >> m;
-
+  string letters = "ACTG";
   vector<string> board(n);
   for (int i = 0; i < n; i++) cin >> board[i];
-  vector<string> perms = get_permutations(4);
   vector<string> copy_board = board;
   vector<string> ans;
-  int min_changes = n * m;
-  for (string perm : perms) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        int index = (2 * (i % 2) + j) % 4;
-        copy_board[i][j] = perm[index];
+
+  int min_total_changes = n * m;
+  for (char c1 : letters) {
+    for (char c2 : letters) {
+      if (c2 <= c1) continue;
+      for (char c3 : letters) {
+        if (c1 == c3 || c2 == c3) continue;
+        for (char c4 : letters) {
+          if (c1 == c4 || c2 == c4 || c4 <= c3) continue;
+          vector<vector<char>> cs = {{c1, c2}, {c3, c4}};
+          int total_changes = 0;
+          for (int i = 0; i < n; i++) {
+            int min_row_changes = m + 1;
+            for (int order = 0; order < 2; order++) {
+              int row_changes = 0;
+              string tmp(m, '.');
+              for (int j = 0; j < m; j++) {
+                tmp[j] = cs[i % 2][(j + order) % 2];
+                if (tmp[j] != board[i][j]) {
+                  row_changes++;
+                }
+              }
+              if (row_changes < min_row_changes) {
+                copy_board[i] = tmp;
+                min_row_changes = row_changes;
+              }
+            }
+            total_changes += min_row_changes;
+          }
+          if (total_changes < min_total_changes) {
+            min_total_changes = total_changes;
+            ans = copy_board;
+          }
+        }
       }
     }
-    int changes = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (copy_board[i][j] != board[i][j]) changes++;
+  }
+
+  for (char c1 : letters) {
+    for (char c2 : letters) {
+      if (c2 <= c1) continue;
+      for (char c3 : letters) {
+        if (c1 == c3 || c2 == c3) continue;
+        for (char c4 : letters) {
+          if (c1 == c4 || c2 == c4 || c4 <= c3) continue;
+          vector<vector<char>> cs = {{c1, c2}, {c3, c4}};
+          int total_changes = 0;
+          for (int i = 0; i < m; i++) {
+            int min_row_changes = n + 1;
+            for (int order = 0; order < 2; order++) {
+              int row_changes = 0;
+              string tmp(n, '.');
+              for (int j = 0; j < n; j++) {
+                tmp[j] = cs[i % 2][(j + order) % 2];
+                if (tmp[j] != board[j][i]) {
+                  row_changes++;
+                }
+              }
+              if (row_changes < min_row_changes) {
+                for (int k = 0; k < n; k++) copy_board[k][i] = tmp[k];
+                min_row_changes = row_changes;
+              }
+            }
+            total_changes += min_row_changes;
+          }
+          if (total_changes < min_total_changes) {
+            min_total_changes = total_changes;
+            ans = copy_board;
+          }
+        }
       }
     }
-    if (changes < min_changes) {
-      ans = copy_board;
-      min_changes = changes;
-    }
   }
-  
-  for (int i = 0; i < n; i++) {
-    cout << ans[i] << endl;
+
+  for (string s : ans) {
+    cout << s << endl;
   }
-  
 }
